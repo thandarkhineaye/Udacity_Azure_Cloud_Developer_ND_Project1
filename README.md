4e0773a75f6e08303295521770ad3f576fbe8362
Thandar Khine Aye <thandarkhineaye92@gmail.com>
2021-04-25 21:26:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Udacity_ND_Pj1
+ a simple Content Management System (CMS) for articles
---
+++ application.py
@@ -0,0 +1,14 @@
+"""
+This script runs the FlaskWebProject application using a development server.
+"""
+
+from os import environ
+from FlaskWebProject import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '3000'))
+    except ValueError:
+        PORT = 3000
+    app.run(HOST, PORT, ssl_context='adhoc')
